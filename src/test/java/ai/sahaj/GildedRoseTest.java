13b9bd4eaff41e956c46c04166fbdc41a0a05ecd
Gracil Benedict <gracilb@sahaj.ai>
2025-03-11 09:47:52 +0530

    [Arun | Gracil] Add. tests for backstage passes

--- src/test/java/ai/sahaj/GildedRoseTest.java
+++ src/test/java/ai/sahaj/GildedRoseTest.java
@@ -1,5 +1,6 @@
 package ai.sahaj;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,10 +9,61 @@ class GildedRoseTest {
 
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("fixme", app.items[0].name);
+    }
+
+    @Nested
+    class BackstagePassesTests {
+        @Test
+        void shouldIncrementQualityByOneWhenSellInIsGreaterThanTen() {
+            Item backstagePasses = new Item("Backstage passes to a TAFKAL80ETC concert", 11, 35);
+            GildedRose gildedRose = new GildedRose(new Item[]{backstagePasses});
+            int expectedQuality = 36;
+
+            gildedRose.updateQuality();
+            int actualQuality = backstagePasses.quality;
+
+            assertEquals(expectedQuality, actualQuality);
+        }
+
+        @Test
+        void shouldIncrementQualityByTwoWhenSellInIsGreaterThanOrEqualToSixAndLessThanOrEqualToTen() {
+            Item backstagePasses = new Item("Backstage passes to a TAFKAL80ETC concert", 7, 35);
+            GildedRose gildedRose = new GildedRose(new Item[]{backstagePasses});
+            int expectedQuality = 37;
+
+            gildedRose.updateQuality();
+            int actualQuality = backstagePasses.quality;
+
+            assertEquals(expectedQuality, actualQuality);
+        }
+
+        @Test
+        void shouldIncrementQualityByThreeWhenSellInIsGreaterThanOrEqualToOneAndLessThanOrEqualToFive() {
+            Item backstagePasses = new Item("Backstage passes to a TAFKAL80ETC concert", 3, 35);
+            GildedRose gildedRose = new GildedRose(new Item[]{backstagePasses});
+            int expectedQuality = 38;
+
+            gildedRose.updateQuality();
+            int actualQuality = backstagePasses.quality;
+
+            assertEquals(expectedQuality, actualQuality); //Arrange Act Assert
+        }
+
+        @Test
+        void shouldSetQualityToZeroWhenItemIsExpired() {
+            Item backstagePasses = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 35);
+            GildedRose gildedRose = new GildedRose(new Item[]{backstagePasses});
+            int expectedQuality = 0;
+
+            gildedRose.updateQuality();
+            int actualQuality = backstagePasses.quality;
+
+            assertEquals(expectedQuality, actualQuality);
+        }
     }
 
 }
