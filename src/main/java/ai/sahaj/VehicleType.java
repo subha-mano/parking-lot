288e1a450d9ff6caaa5e67590f6219e770a7548e
Subha Manoharan <subham@Subhas-MacBook-Pro.local>
2025-03-18 09:29:16 +0530

    Move rates out of VehicleType as they differ

--- src/main/java/ai/sahaj/VehicleType.java
+++ src/main/java/ai/sahaj/VehicleType.java
@@ -1,12 +1,7 @@
 package ai.sahaj;
 
 public enum VehicleType {
-    BIKE(10),
-    CAR(20),
-    BUS(50);
-    public final int rate;
-
-    VehicleType(int rate) {
-        this.rate = rate;
-    }
+    BIKE(),
+    CAR(),
+    BUS();
 }
