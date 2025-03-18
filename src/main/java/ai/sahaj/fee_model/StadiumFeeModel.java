f36d5157eae414c158b60ee16b70161c80dcfeec
Subha Manoharan <subham@Subhas-MacBook-Pro.local>
2025-03-18 18:14:54 +0530

    Handle boundary cases for stadium parking

--- src/main/java/ai/sahaj/fee_model/StadiumFeeModel.java
+++ src/main/java/ai/sahaj/fee_model/StadiumFeeModel.java
@@ -5,22 +5,22 @@ import ai.sahaj.VehicleType;
 public class StadiumFeeModel implements FeeModel {
     @Override
     public int fees(VehicleType vehicleType, long minutes) {
-        int hours = (int) Math.ceil(minutes / 60.0);
+        float hours = (float) minutes / 60;
         int fees = 0;
         if(vehicleType.equals(VehicleType.BIKE)){
-            if(hours <= 4)
+            if(hours < 4)
                 fees += 30;
-            else if(hours <= 12)
+            else if(hours < 12)
                 fees += 30 + 60;
             else
-                fees = 30 + 60 + (100 * (hours -12));
+                fees = 30 + 60 + (100 * ((int) Math.ceil(hours) -12));
         } else if(vehicleType.equals(VehicleType.CAR)){
-            if(hours <= 4)
+            if(hours < 4)
                 fees += 60;
-            else if(hours <= 12)
+            else if(hours < 12)
                 fees += 60 + 120;
             else
-                fees = 60 + 120 + (200 * (hours -12));
+                fees = 60 + 120 + (200 * ((int)Math.ceil(hours) - 12));
         }
         return fees;
     }
