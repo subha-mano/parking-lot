e09f483e115fc2a566c519d12cee72f7a283f0f1
Subha Manoharan <subham@Subhas-MacBook-Pro.local>
2025-03-19 09:19:10 +0530

    Add rules for Mall model

--- src/main/java/ai/sahaj/fee_model/MallFeeModel.java
+++ src/main/java/ai/sahaj/fee_model/MallFeeModel.java
@@ -2,15 +2,29 @@ package ai.sahaj.fee_model;
 
 import ai.sahaj.VehicleType;
 import ai.sahaj.fee_model.rate.PerHourRate;
+import ai.sahaj.utils.Interval;
+
+import java.util.Arrays;
 
 import static ai.sahaj.VehicleType.*;
 
 public class MallFeeModel implements FeeModel {
     public int fees(VehicleType vehicleType, long minutes) {
         int hours = (int) Math.ceil(minutes / 60.0);
-        if (vehicleType == BIKE) return new PerHourRate(10).fees(hours);
-        else if(vehicleType == CAR) return new PerHourRate(20).fees(hours);
-        else if(vehicleType == BUS ) return new PerHourRate(50).fees(hours);
-        return 0;
+        Rule[] rules = new Rule[]{
+            new Rule(new Interval(0, Integer.MAX_VALUE), new PerHourRate(10), BIKE),
+            new Rule(new Interval(0, Integer.MAX_VALUE), new PerHourRate(20), CAR),
+            new Rule(new Interval(0, Integer.MAX_VALUE), new PerHourRate(50), BUS)
+        };
+        return fees(rules, hours, vehicleType);
     }
+
+    private int fees(Rule[] rules, float hours, VehicleType vehicleType) {
+        return Arrays.stream(rules)
+                .filter(rule -> rule.isMatch(hours, vehicleType))
+                .findFirst().map(rule -> rule.fees(hours))
+                .orElse(0);
+    }
+
 }
+
